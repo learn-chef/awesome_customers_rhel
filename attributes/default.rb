0f2b2ba061e0daf4581ebc316d9eb1895ff9deaa
tpetchel <tpetchel@gmail.com>
2016-02-04 09:42:20 -0500

    revision

---
+++ attributes/default.rb
@@ -0,0 +1,16 @@
+default['firewall']['allow_ssh'] = true
+default['firewall']['firewalld']['permanent'] = false
+default['firewall']['ipv6_enabled'] = false
+default['awesome_customers_rhel']['open_ports'] = 80
+
+default['awesome_customers_rhel']['user'] = 'web_admin'
+default['awesome_customers_rhel']['group'] = 'web_admin'
+default['awesome_customers_rhel']['document_root'] = '/var/www/customers/public_html'
+
+default['awesome_customers_rhel']['secret_file'] = '/etc/chef/encrypted_data_bag_secret'
+
+default['awesome_customers_rhel']['database']['dbname'] = 'my_company'
+default['awesome_customers_rhel']['database']['host'] = '127.0.0.1'
+default['awesome_customers_rhel']['database']['root_username'] = 'root'
+default['awesome_customers_rhel']['database']['admin_username'] = 'db_admin'
+default['awesome_customers_rhel']['database']['create_tables_script'] ='/tmp/create-tables.sql'
---
+++ recipes/web.rb
@@ -0,0 +1,48 @@
+#
+# Cookbook Name:: awesome_customers_rhel
+# Recipe:: web
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+# Install Apache and start the service.
+httpd_service 'customers' do
+  mpm 'prefork'
+  action [:create, :start]
+end
+
+# Add the site configuration.
+httpd_config 'customers' do
+  instance 'customers'
+  source 'customers.conf.erb'
+  notifies :restart, 'httpd_service[customers]'
+end
+
+# Create the document root directory.
+directory node['awesome_customers_rhel']['document_root'] do
+  recursive true
+end
+
+# Load the secrets file and the encrypted data bag item that holds the root password.
+password_secret = Chef::EncryptedDataBagItem.load_secret(node['awesome_customers_rhel']['secret_file'])
+password_data_bag_item = Chef::EncryptedDataBagItem.load('database_passwords', 'mysql_customers', password_secret)
+
+# Write the home page.
+template "#{node['awesome_customers_rhel']['document_root']}/index.php" do
+  source 'index.php.erb'
+  mode '0644'
+  owner node['awesome_customers_rhel']['user']
+  group node['awesome_customers_rhel']['group']
+  variables(
+    :database_password => password_data_bag_item['admin_password']
+  )
+end
+
+# Install the mod_php Apache module.
+httpd_module 'php' do
+  instance 'customers'
+end
+
+# Install php-mysql.
+package 'php-mysql' do
+  action :install
+  notifies :restart, 'httpd_service[customers]'
+end
